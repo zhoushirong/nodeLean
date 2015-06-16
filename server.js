5aa3567bccffc0e2a756aecf72665f722dc5dccd
zsrcat <1091263031@qq.com>
2015-06-16 23:41:35 +0800

    引入ejs

--- server.js
+++ server.js
@@ -6,53 +6,56 @@ var path=require('path');//引用path模块
 var hbs=require('hbs');
 //加载数据模块
 var bodyParser = require('body-parser')
-var blogEngine=require('./control/blog');
+var ejsTemp=require('ejs');
 
+//运行ejs模块
+app.engine('.html',ejsTemp.__express);
 // 设定views变量，意为视图存放的目录
-app.set('views', path.join(__dirname, '/views'));
+app.set('views',path.join(__dirname,'views'));
+//指定模板文件的后缀名为html
+app.set('view engine','html'); 
 
 // 设定静态文件目录，比如本地文件,目录为demo/public/images，访问网址则显示为http://localhost:3000/images
 app.use(express.static(path.join(__dirname, '/')));
 
-//指定模板文件的后缀名为html
-app.set('view engine','html'); 
 
-//运行hbs模块
-app.engine('html',hbs.__express);
-app.use(bodyParser());
+
+//app.use(bodyParser());
 
 var router=express.Router();
 
 //router.get("path","")path：指新的访问地址
 //res.render('index') 就是指，把子目录views下面的index.html文件，交给模板引擎hbs渲染。
 router.get('/',function(req,res){
-	blogEngine.getBlogEntries(function(d){
-		res.render('visitor/index',{title:"最近文章", entries:d});
+	res.render('visitor/index',{
+		title:"最近文章",
+		time:"2012/03/02"
 	});
-	
-});
-router.get('/article/:id', function(req, res) {
-	blogEngine.getBlogEntry(req.params.id,function(d){
-		res.render('visitor/article',{title:d.title,blog:d});
-	})
+	res.end();
 });
 
-router.get('/about',function(req,res){
-	res.render('user/about', {title:"自我介绍"});
- });
-router.get('/login',function(req,res){
-	res.render('user/login', {title:"登录"});
- });
-router.get('/signup',function(req,res){
-	res.render('user/signup', {title:"注册"});
- });
+// router.get('/article/:id', function(req, res) {
+// 	ejs.getBlogEntry(req.params.id,function(d){
+// 		res.render('visitor/article',{title:d.title,blog:d});
+// 	})
+// });
 
-app.get('/message',function(req,res,next){ //  get请求返回json
-	var url=req.url;
-	var args=url.replace(/.+args=/,'');
+// router.get('/about',function(req,res){
+// 	res.render('user/about', {title:"自我介绍"});
+//  });
+// router.get('/login',function(req,res){
+// 	res.render('user/login', {title:"登录"});
+//  });
+// router.get('/signup',function(req,res){
+// 	res.render('user/signup', {title:"注册"});
+//  });
 
-    res.send({status:'json',"args":args});
-});
+// app.get('/message',function(req,res,next){ //  get请求返回json
+// 	var url=req.url;
+// 	var args=url.replace(/.+args=/,'');
+
+//     res.send({status:'json',"args":args});
+// });
 
 app.use('/',router);
 app.listen(3000);
