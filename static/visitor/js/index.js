1457cdbae8a7e310b775124a99a7e29e3e0115b0
zsrcat <1091263031@qq.com>
2015-06-17 22:00:09 +0800

    莫名其妙的冲突

--- static/visitor/js/index.js
+++ static/visitor/js/index.js
@@ -1,20 +1,20 @@
-$(document).ready(function(){
-	$("#loadMore").click(function(){
-		getMore();
-	});
+// $(document).ready(function(){
+// 	$("#loadMore").click(function(){
+// 		getMore();
+// 	});
 
-	//获取更多文章
-	function getMore(){
-		$.get("http://127.0.0.1:3001/message?args=[1234]",function(d){
-			//alert(d);
-		});
-		// $.ajax({
-		//   type: 'POST',
-		//   url: "http://127.0.0.1:3001",
-		//   ars:"message"
-		//   data: {"id":1,"data":123},
-		//   success: function(d){},
-		//   dataType: "json"
-		// });
-	}
-});
+// 	//获取更多文章
+// 	function getMore(){
+// 		$.get("http://127.0.0.1:3001/message?args=[1234]",function(d){
+// 			//alert(d);
+// 		});
+// 		$.ajax({
+// 		  type: 'POST',
+// 		  url: "http://127.0.0.1:3001",
+// 		  ars:"message"
+// 		  data: {"id":1,"data":123},
+// 		  success: function(d){},
+// 		  dataType: "json"
+// 		});
+// 	}
+// });
