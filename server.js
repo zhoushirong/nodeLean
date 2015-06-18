0816d559c40b415a5298ad4f73d666f88cb78db5
zsrcat <1091263031@qq.com>
2015-06-18 22:07:18 +0800

    注册事件

--- server.js
+++ server.js
@@ -4,10 +4,16 @@ var path=require("path");//引用path模块
 //加载数据模块
 var ejsTemp=require("ejs");
 var url=require("url");
+var bodyParser = require("body-parser"); 
 //根据参数选择对象
 var selectObjById=require("self-pub").getTrueAtc;
+//处理接收到的数据
+var dealData=require("deal-data");
+
 
 //**********************加载模拟数据**************************
+app.use(bodyParser.urlencoded({ extended: false })); 
+
 var getArticles=require("./data/articles");
 
 //运行ejs模块
@@ -19,16 +25,18 @@ app.set("view engine","html");
 
 // 设定静态文件目录，比如本地文件,目录为demo/public/images，访问网址则显示为http://localhost:3000/images
 app.use(express.static(path.join(__dirname, "/")));
-
 var router=express.Router();
 
-router.get("/",function(req,res){//文章列表 & 首页
+//文章列表 & 首页
+router.get("/",function(req,res){
 	getArticles(function(data){
 		res.render("visitor/index",{articles:data});
 		res.end();
 	});
 });
-router.get("/page",function(req,res){//文章详情页
+
+//文章详情页
+router.get("/page",function(req,res){
 	var articleId=parseInt(url.parse(req.url,true).query.id);
 	getArticles(function(data){
 		res.render("visitor/article",{article:selectObjById(data,"id",articleId)});
@@ -44,22 +52,35 @@ router.get("/page",function(req,res){//文章详情页
 // 	res.render("user/login", {title:"登录"});
 //  });
 
-router.get("/signup",function(req,res){//注册用户
+//注册页面
+router.get("/signup",function(req,res){
 	res.render("user/signup", {title:"注册"});
+	res.end();
  });
 
-app.get("/message",function(req,res,next){ //  get请求返回json
+//  get请求返回json
+app.get("/message",function(req,res,next){ 
 	var args=url.parse(req.url,true).query.args;
     res.send({status:"jsonget","args":args});
     res.end();
 });
 
-app.post("/message",function(req,res,next){ //  post请求返回json
+//  post请求返回json
+app.post("/message",function(req,res,next){ 
 	var args=url.parse(req.url,true).query.args;
     res.send({status:"jsonpost","args":args});
     res.end();
 });
 
+//  注册事件
+app.post("/register",function(req,res,next){ 
+    res.send({status:"1","data":"注册成功！"});
+    var user_name=req.body.username;  
+  	var password=req.body.password;
+  	var data={"username":user_name,"password":password};
+    dealData.signup(data);
+    res.end();
+});
 
 app.use("/",router);
 app.listen(3000);
