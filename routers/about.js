b93048cde4fe6fcb99d5fdbf3706a55e6b92c3f6
周世熔 <zhoushirong@kugou.net>
2015-06-19 11:06:46 +0800

    删除不必要的文件

--- routers/about.js
+++
@@ -1,9 +0,0 @@
-function About(){
-    this.exec = function(route, req, res){
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        res.end('This is </b>About Me</b>');
-    }
-}
-
-module.exports = new About();
--- routers/contact.js
+++
@@ -1,9 +0,0 @@
-function Contact(){
-    this.exec = function(route, req, res){
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        res.end('This is </b>Contact Page</b>');
-    }
-}
-
-module.exports = new Contact();
