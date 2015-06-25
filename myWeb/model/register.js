f8a186ce49aeeccf7af48d3613165cf800c2659f
周世熔 <zhoushirong@kugou.net>
2015-06-25 16:41:30 +0800

    注册问题解决

--- myWeb/model/register.js
+++ myWeb/model/register.js
@@ -4,54 +4,37 @@
 //**********************************
 var mysql=require("mysql");
 var config=require("../config/config");
-var connection = mysql.createConnection(config.mysqlConfig());
 
-//检测是否已经注册
-function checkHas(userArr,callback){
-	//var checkUser="SELECT * FROM 'user' WHERE userName='"+userArr[0]+"'";
-	var checkUser = "SELECT * FROM user where userName = '"+userArr[0]+"'";
-	connection.query(checkUser, function(err, rows, fields) {
-		if (err) throw err;
-		console.log('checkUser from user success!');
-		console.log(rows+" || "+rows.length);
-		if(rows.length > 0){
-			console.log("已经注册过了！");
-			connection.end();
-		}else{
-			if(typeof callback === "function"){
-				callback();
-			}
-		}
-	});
-}
 module.exports=function(userArr){
+	var connection = mysql.createConnection(config.mysqlConfig());
 	connection.connect();
 	var checkUser = "SELECT * FROM user where userName = '"+userArr[0]+"'";
 	connection.query(checkUser, function(err, rows, fields) {
 		if (err) throw err;
 		console.log('checkUser from user success!');
-		console.log(rows+" || "+rows.length);
+		console.log(userArr[0]+" | "+rows+" | "+rows.length);
 		if(rows.length > 0){
 			console.log("已经注册过了！");
+			connection.end();
 		}else{
-			if(typeof callback === "function"){
-				var inserUser="INSERT INTO user(userName,password,sex,intro,regTime)"
-						+" VALUES("
-						+"'"+userArr[0]+"',"
-						+"'"+userArr[1]+"',"
-						+"'"+userArr[2]+"',"
-						+"'"+userArr[3]+"',"
-						+"'"+userArr[4]+"')";
+			connection.end();
+			var connection2 = mysql.createConnection(config.mysqlConfig());
+			connection2.connect();
+			var inserUser="INSERT INTO user(userName,password,sex,intro,regTime)"
+					+" VALUES("
+					+"'"+userArr[0]+"',"
+					+"'"+userArr[1]+"',"
+					+"'"+userArr[2]+"',"
+					+"'"+userArr[3]+"',"
+					+"'"+userArr[4]+"')";
 
-				connection.query(inserUser, function(err, rows, fields) {
-					if (err) throw err;
-					console.log('insert usertable success!');
-					console.log("注册成功！");
-					connection.end();
-				});
-			}
+			connection2.query(inserUser, function(err, rows, fields) {
+				if (err) throw err;
+				console.log('insert usertable success!');
+				console.log("注册成功！");
+				connection2.end();
+			});
 		}
-		connection.end();
 	});
 }
 
